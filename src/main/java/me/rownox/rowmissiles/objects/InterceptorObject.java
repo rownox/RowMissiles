eb7257962b710b9f9881af9932621c39b94a791e
rownox <90977516+rownox@users.noreply.github.com>
2024-11-01 19:58:06 -0400

    Initial interceptor object.

---
+++ src/main/java/me/rownox/rowmissiles/objects/InterceptorObject.java
@@ -0,0 +1,12 @@
+package me.rownox.rowmissiles.objects;
+
+public class InterceptorObject {
+    private final int radius;
+    private final String name;
+
+    public InterceptorObject(int radius, String name) {
+
+        this.radius = radius;
+        this.name = name;
+    }
+}
